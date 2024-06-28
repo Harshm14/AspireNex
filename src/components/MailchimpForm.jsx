9f8a213b0bc48c1add716790166810671feae7d7
Harshm14 <harsh.mittal0142@gmail.com>
2024-06-28 20:30:23 +0530

    first commit

---
+++ src/components/MailchimpForm.jsx
@@ -0,0 +1,22 @@
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { Newsletter } from "./Newsletter";
+
+export const MailchimpForm = () => {
+  const postUrl = `${import.meta.env.VITE_MAILCHIMP_URL}?u=${import.meta.env.VITE_MAILCHIMP_U}&id=${import.meta.env.VITE_MAILCHIMP_ID}`;
+
+  // Optional: Log the postUrl to verify it's correctly formed (remove in production)
+  console.log(postUrl);
+
+  return (
+    <MailchimpSubscribe
+      url={postUrl}
+      render={({ subscribe, status, message }) => (
+        <Newsletter
+          status={status}
+          message={message}
+          onValidated={formData => subscribe(formData)}
+        />
+      )}
+    />
+  );
+};
