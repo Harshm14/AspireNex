0d56133f247c88b09af7ae14f79c6cd0565c95ae
Harshm14 <harsh.mittal0142@gmail.com>
2024-06-28 20:41:11 +0530

    1

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -57,7 +57,13 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Harsh Mittal`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer"  ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>I’m a dedicated and enthusiastic B. Tech student specializing in Information Technology at Maharaja Agrasen Institute of
+Technology. My academic journey has been marked by consistently
+high grades, both in secondary and higher secondary education, laying
+a strong foundation for my current pursuits. Through internships and
+projects, I’ve gained valuable hands-on experience, honing my skills in
+programming languages like C, C++, and Python, which I’ve applied in
+creating games, management systems, and web development projects..</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
